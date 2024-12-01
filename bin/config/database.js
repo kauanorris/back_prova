0a3ca612e0378415ed69afeb0c5bf5632a079a5a
kauanorris <kauafini13@hotmail.com>
2024-12-01 14:51:03 -0300

    Revert "commit"
    This reverts commit 71a952b988f65ea096d0aa8805612c628265a975.

--- bin/config/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('ecommerce_db', 'root', 'sqx105019', {
-  host: 'localhost',
-  dialect: 'mysql' 
-});
-
-module.exports = sequelize;
